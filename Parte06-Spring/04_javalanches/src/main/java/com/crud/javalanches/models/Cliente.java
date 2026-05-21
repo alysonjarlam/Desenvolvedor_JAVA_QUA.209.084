9ad47f997dfd26e55c290463f8f22b5e73ff9763
alysonjarlam <alysonjarlam@gmail.com>
2026-05-20 21:41:38 -0300

    aula db_criando_entidad_20-05-26

--- Parte06-Spring/04_javalanches/src/main/java/com/crud/javalanches/models/Cliente.java
+++ Parte06-Spring/04_javalanches/src/main/java/com/crud/javalanches/models/Cliente.java
@@ -2,21 +2,80 @@ package com.crud.javalanches.models;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.Column; //@Collumn
+import jakarta.persistence.Entity; // veio do strategy = GenerationType.AUTO
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 // para criar classe, ela sempre deve iniciar com letra maiuscula
 @Entity
 public class Cliente {
     // atributos
-    private long codigo_cliente;
+    private static long serialVersionUID = 1L; // faz verificação da chave primaria
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long codigoCliente;
+    @Column(nullable = false)
     private String nome;
+    @Column(nullable = false, unique = true, length = 14)
     private String cpf;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false, unique = true)
     private String telefone;
+    @Column(nullable = false)
     private LocalDate datadeNascimento;
 
 
+    // constructor
     public Cliente() {
     }
+    // getters & setters
+    public long getCodigoCliente() {
+        return this.codigoCliente;
+    }
 
-    
+    public void setCodigoCliente(long codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDatadeNascimento() {
+        return this.datadeNascimento;
+    }
+
+    public void setDatadeNascimento(LocalDate datadeNascimento) {
+        this.datadeNascimento = datadeNascimento;
+    }
 }
