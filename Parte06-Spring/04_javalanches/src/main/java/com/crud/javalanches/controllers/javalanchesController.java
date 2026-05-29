07773503438c39f751c121f16174b0918e9c5bc5
alysonjarlam <alysonjarlam@gmail.com>
2026-05-28 21:47:43 -0300

    2º comit de 28/05/2026

--- Parte06-Spring/04_javalanches/src/main/java/com/crud/javalanches/controllers/javalanchesController.java
+++ Parte06-Spring/04_javalanches/src/main/java/com/crud/javalanches/controllers/javalanchesController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.crud.javalanches.models.Categoria;
+import com.crud.javalanches.models.Produto;
 import com.crud.javalanches.repository.CategoriaRepository;
+import com.crud.javalanches.repository.ProdutoRepository;
 
 import ch.qos.logback.core.model.Model;
 
@@ -15,6 +17,9 @@ import ch.qos.logback.core.model.Model;
 public class javalanchesController {
     @Autowired
     private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private ProdutoRepository
 
     @GetMapping("/")
     public String index() {
@@ -32,8 +37,16 @@ public class javalanchesController {
 
     }
     @GetMapping("/novoProduto")
-    public String novoProduto(Model model ) {
+    public String novoProduto(Model model) {
         model.addAttribute("Categoria", categoriaRepository.findAll());
         return "novo_produto";
+    }
+    
+    @PostMapping("/novoProduto")
+    public String novoProduto(Produto produto, @RequestParam("catedoriaID") Long categoriaID) {
+        Categoria categoria = categoriaRepository.findById(categoriaID).orElse(null);
+        produto.setCategoria(categoria);
+        produtoRepository.save(produto);
+        return "produto_sucesso"
     }
 }
---
+++ Parte06-Spring/04_javalanches/src/main/java/com/crud/javalanches/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.crud.javalanches.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long{
+
+}
